b16fe4ca4c1daa23076038446a539cab7666577b
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-08-25 20:38:08 +0530

    Errors fixed

--- src/authentication/LoginForm.jsx
+++ src/authentication/LoginForm.jsx
@@ -1,29 +1,62 @@
 import React from "react";
 import "./LoginForm.css";
-import AppleIcon from '@mui/icons-material/Apple';
-import GoogleIcon from '@mui/icons-material/Google';
-import SignUp from "./SignUp";
+import AppleIcon from "@mui/icons-material/Apple";
+import GoogleIcon from "@mui/icons-material/Google";
 import { Link } from "react-router-dom";
 
 export default function LoginForm({ onClose }) {
     return (
         <div className="auth-modal">
             <div className="auth-box">
+                {/* Close Button */}
                 <button className="close-btn" onClick={onClose}>✖</button>
+
                 <h2>Login</h2>
 
-                <input type="email" placeholder="Email" name="email" />
-                <input type="password" placeholder="Password" name="password" />
-                <button className="login-btn">Login</button>
+                <form
+                    className="login-form"
+                >
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                            autoComplete="email"
+                            required
+                        />
+                    </div>
 
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            autoComplete="current-password"
+                            required
+                        />
+                    </div>
+
+                    <button type="submit" className="login-btn">Login</button>
+                </form>
+
+                {/* Social Login */}
                 <div className="social-login">
-                    <button className="google-btn"><GoogleIcon fontSize="medium" /></button>
-                    <button className="apple-btn"><AppleIcon fontSize="medium" /></button>
+                    <button type="button" className="google-btn">
+                        <GoogleIcon fontSize="medium" /> Google
+                    </button>
+                    <button type="button" className="apple-btn">
+                        <AppleIcon fontSize="medium" /> Apple
+                    </button>
                 </div>
 
+                {/* Links */}
                 <div className="auth-links">
-                    <span>Don't have an account? <Link to="/signup">Sign Up</Link></span>
-                    <span><a href="#">Forgot?</a></span>
+                    <span>
+                        Don't have an account? <Link to="/signup">Sign Up</Link>
+                    </span>
+                    <span>
+                        <Link to="/forgot-password">Forgot Password?</Link>
+                    </span>
                 </div>
             </div>
         </div>
