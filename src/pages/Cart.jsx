48057a5de0255e182195b7a28ce109317ddb173e
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-08-25 21:00:44 +0530

    Issue fixed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Cart.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import LoginForm from "../authentication/LoginForm";
 
 export default function Cart() {
+    const [showLoginPage, setShowLoginPage] = useState(false);
+
     return (
         <>
             <Navbar />
@@ -12,11 +15,14 @@ export default function Cart() {
                 <div className="cart-box">
                     <h2>Missing Cart items?</h2>
                     <p>Login to see the items you added previously</p>
-                    <button className="login-btn">Login</button>
+                    <button className="login-btn" onClick={() => setShowLoginPage(true)} >Login</button>
                 </div>
             </div>
 
             <Footer />
+
+            {/* Show LoginForm modal if user clicks Login */}
+            {showLoginPage && <LoginForm onClose={() => setShowLoginPage(false)} />}
         </>
     );
 }
