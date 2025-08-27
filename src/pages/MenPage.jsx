838b7d23c83956c02ab047d8189f5346d598156e
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-08-27 20:46:18 +0530

    Changed

--- src/pages/MenPage.jsx
+++ src/pages/MenPage.jsx
@@ -18,7 +18,7 @@ export default function MenPage() {
             <Navbar />
 
             <section className="beauty-section">
-                <h1 className="section-title">{title}</h1>
+                <h1 className="page-title">{title}</h1>
                 <div className="beauty-grid">
                     {menProducts.map((product) => (
                         <div className="beauty-card" key={product.id}>
--- src/pages/WomenPage.jsx
+++ src/pages/WomenPage.jsx
@@ -18,7 +18,7 @@ export default function WomenPage() {
             <Navbar />
 
             <section className="beauty-section">
-                <h1 className="section-title">{title}</h1>
+                <h1 className="page-title">{title}</h1>
                 <div className="beauty-grid">
                     {womenProducts.map((product) => (
                         <div className="beauty-card" key={product.id}>
