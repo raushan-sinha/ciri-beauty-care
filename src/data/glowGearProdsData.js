6b34cd96a612019f1091eb5690a06f977741d902
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-01 17:03:20 +0530

    Added images links

--- src/data/glowGearProdsData.js
+++ src/data/glowGearProdsData.js
@@ -2,35 +2,35 @@ const glowGearProduct = [
     {
         products: [
             {
-                src: 'https://scontent.fpat2-7.fna.fbcdn.net/v/t1.15752-9/530689057_1160260389245421_8681899071375772272_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=101&ccb=1-7&_nc_sid=0024fc&_nc_ohc=mLDY4VOKDYMQ7kNvwHAt1Ij&_nc_oc=AdnDoNM3fHY9a9TK1SWqaeZV9V7InTR9yAYGy2VGXKI5GYlAiYVp6Hs9vJoOtTQqnS0&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-7.fna&oh=03_Q7cD3AHVziH0nfS-PUpu4negoCeSri5iAXaxd4_D2eGLNcvGnQ&oe=68CCE6AB',
+                src: 'https://scontent.fpat2-7.fna.fbcdn.net/v/t1.15752-9/590766836_1531248698017373_3213942035257705842_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_ohc=7oue6C37XcwQ7kNvwHCgcBS&_nc_oc=Adkb7V8UdWwALrP3DALZ2WM0iANKgiei0jELHkDieKhtwiWbLXTrdmQzbeSwBma__eI&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-7.fna&oh=03_Q7cD4AFzft34eY8W9rQZqrneA_eZb1NgdwX224SedJB29Ybimw&oe=6954F8F1',
                 alt: 'Facial Steamer',
                 name: 'Nano Facial Steamer',
                 desc: 'Opens pores, hydrates deeply, and preps skin for treatments.',
                 price: '₹2,499'
             },
             {
-                src: 'https://scontent.fpat2-7.fna.fbcdn.net/v/t1.15752-9/531843050_2590249067989175_6903735363263939273_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_ohc=iDnWzEEfaAUQ7kNvwEpCCz-&_nc_oc=Adnj0EjZtDymbHHPLB3P7h7yR4ZT0DrdCEqjpxfkXf7dv2gUxGkyNZjK7hwg3c40z4o&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-7.fna&oh=03_Q7cD3AEbEjGn40rYwgzIb8E4pxEi3RcL4z1S_JtXCoo0pj5cMA&oe=68CD194D',
+                src: 'https://scontent.fpat2-8.fna.fbcdn.net/v/t1.15752-9/592238950_3104127549975272_421301388108513575_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=103&ccb=1-7&_nc_sid=0024fc&_nc_ohc=dERwQ5HDpfMQ7kNvwEMK_9g&_nc_oc=AdlghPEwwPnKgST2CsSw4RJa_ab98yrQcmXyI86hy4a0Shpha9o-zFV_CWdTcr2WgwQ&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-8.fna&oh=03_Q7cD4AEkIk8lwwda0qMTxJTLffpXDVY1ocRhE-JCuKDDIb5pTg&oe=6954F3F4',
                 alt: 'Face Massager',
                 name: '3D Face Massager',
                 desc: 'Relieves tension, boosts circulation, and enhances skin firmness.',
                 price: '₹1,799'
             },
             {
-                src: 'https://scontent.fpat2-2.fna.fbcdn.net/v/t1.15752-9/531250815_1779050929372003_7225759504309140462_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=110&ccb=1-7&_nc_sid=0024fc&_nc_ohc=7yNMqUrBEuIQ7kNvwE7lBO6&_nc_oc=AdmD9Vl-d-p1hLYFaVLeyvD9BjZIZhVl62hWrDNUwmtu-I65DTLzIz9bsbhmbKKqz0A&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-2.fna&oh=03_Q7cD3AHJKagVwibJ_9RLOOtkhZCyRujlVyoHFJKb1VJbQvOKpQ&oe=68CCECFD',
+                src: 'https://scontent.fpat2-5.fna.fbcdn.net/v/t1.15752-9/589951519_732207132650011_726143565637617953_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=104&ccb=1-7&_nc_sid=0024fc&_nc_ohc=EkpP54mMbhYQ7kNvwFvhNnt&_nc_oc=Adk4NAW1bmeMQT7mXpDjvzHbwba8KBSLqRgsEQAmx_zDXc7zgnTxOMwluDNEqkfA6gk&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-5.fna&oh=03_Q7cD4AGJu0-_4Lx7_yK0kMkwj8MOPsjONxxwm9ZH8gTCOB5g5g&oe=6954F46F',
                 alt: 'Derma Roller',
                 name: 'Titanium Derma Roller',
                 desc: 'Stimulates collagen, reduces fine lines, and improves absorption of serums.',
                 price: '₹1,299'
             },
             {
-                src: 'https://scontent.fpat2-5.fna.fbcdn.net/v/t1.15752-9/535467827_2271751149929248_8403032998203094641_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=104&ccb=1-7&_nc_sid=0024fc&_nc_ohc=dkVJV9WNofkQ7kNvwFvR2Tc&_nc_oc=Adlcl4papRNTeqy4OxpAsweZ_CVMx68Wm6sKPnhuYPowsOTKct3Ekpztf9MKDOZaNzA&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-5.fna&oh=03_Q7cD3AEm8tju1cmcUkLc7894ZxNeI3aEjesr1hHRo67IEE2qsw&oe=68CCF592',
+                src: 'https://scontent.fpat2-8.fna.fbcdn.net/v/t1.15752-9/577815566_1388112899596123_7724947068208971905_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=103&ccb=1-7&_nc_sid=0024fc&_nc_ohc=6zf5VAFZneMQ7kNvwG_n3en&_nc_oc=AdmdAmj0L8ZkDfnVoU94VP51kcusZWYYXWqKrczpeZbmdHCOiFVMEicXlo4_az-mVsE&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-8.fna&oh=03_Q7cD4AGabTIsCOPtvFQA9WoG9CTwRPl9nLRBIrKDANJaGUWYzQ&oe=6954D85C',
                 alt: 'LED Therapy Mask',
                 name: 'LED Therapy Mask',
                 desc: 'Multi-color LED light therapy to reduce acne, wrinkles, and dark spots.',
                 price: '₹3,999'
             },
             {
-                src: 'https://scontent.fpat2-5.fna.fbcdn.net/v/t1.15752-9/531761395_1954760571942711_7839937427859610337_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=104&ccb=1-7&_nc_sid=0024fc&_nc_ohc=UoOB1FNykG4Q7kNvwGqvcWE&_nc_oc=AdnFuaXUbLMZlVIlYtGvrkEO0nFfC6ZOzY1zc0kQGXTNzwG2Ql1NReVCdkaFdSZrTQA&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-5.fna&oh=03_Q7cD3AFemV3diwvJRHC8HfgkO-kVa1l26VVJi0Yf7gnpJBzHXw&oe=68CD15F3',
+                src: 'https://scontent.fpat2-8.fna.fbcdn.net/v/t1.15752-9/591836835_1181669983408760_3921946454911725981_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=103&ccb=1-7&_nc_sid=0024fc&_nc_ohc=6jvHu0farFsQ7kNvwFCKOCu&_nc_oc=AdkNf7a0hjnAA-4zhspyM-S4yiRQRNeHReAMspJRpEFmKpq1vtTQos6fbRpiVVbNL78&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-8.fna&oh=03_Q7cD4AG6evvcUD0YIi5XzZJvBiQxXgSAC2uJoyFyD7kWFOKR3Q&oe=6955024A',
                 alt: 'Hair Remover',
                 name: 'Painless Hair Remover',
                 desc: 'Safe and gentle device for smooth, hair-free skin at home.',
