058a787b82abf3aab98ef3666fbc237f92a3cd72
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-01 16:40:59 +0530

    Added images links

--- src/data/womenProductsData.js
+++ src/data/womenProductsData.js
@@ -3,37 +3,37 @@ const womenProducts = [
         id: 1,
         name: "Hydrating Facewash",
         price: "₹299",
-        image: "https://scontent.fpat2-2.fna.fbcdn.net/v/t1.15752-9/530712207_1095848189359429_5652692108601680955_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=110&ccb=1-7&_nc_sid=0024fc&_nc_ohc=0lOIUJH_kTkQ7kNvwEKELWU&_nc_oc=AdltHf4ThtlTPAEWZhzxDzI4VESPsJblRtti-MAP1fgH32Qzs9SHY8K5PpXqj2-Pf3g&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-2.fna&oh=03_Q7cD3AGPxScuBHdEpOs480wZAAqc6Fmp8tAxL0g5SLNnqpbHUg&oe=68CBA6F3",
+        image: "https://scontent.fpat2-1.fna.fbcdn.net/v/t1.15752-9/578012880_1150303093935005_1886551342102503911_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=Q_i3p2d0I1QQ7kNvwGbu2fj&_nc_oc=Adl-kWh08lXDuUV5Rf2nC1iEWt79tKSm6UJn1cOAYwnUSeOFn9be_jXTENtTahEXBmk&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-1.fna&oh=03_Q7cD4AGAQ-08Uy7hxv6bfRtdHD4qHviRvKLM6RNiO-7_kprhQw&oe=6954DE73",
     },
     {
         id: 2,
         name: "Brightening Face Cream",
         price: "₹499",
-        image: "https://scontent.fpat2-4.fna.fbcdn.net/v/t1.15752-9/532546943_3210668469092772_5552019831700380067_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=100&ccb=1-7&_nc_sid=0024fc&_nc_ohc=bCPF59fiAhYQ7kNvwEtEOrC&_nc_oc=AdlxcedHBhrQfSxqnboPy4bERm6wk7O-wu4E2-m30ix0rc_DK-Tjahod1ptkyvLM5-Y&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-4.fna&oh=03_Q7cD3AElm2b0HUnWJew221GzmFWWWR-roirMaCtp3m7YPZbAmw&oe=68CBB2B4",
+        image: "https://scontent.fpat2-3.fna.fbcdn.net/v/t1.15752-9/591219241_2322084188211842_5480980473227752759_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=107&ccb=1-7&_nc_sid=0024fc&_nc_ohc=e1VYw_MPlYsQ7kNvwFLcicw&_nc_oc=AdmXA0k5LD1MgglGgjtj61Tx2u2NAZQ_pF-GXwp2kOF2-zlvpIDc_SEQ8r2h27-yI0M&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-3.fna&oh=03_Q7cD4AFOuCCE4JdsWclFuuSUjPncEeVUnM0pfjn0fsiWBaczgg&oe=6954D6E8",
     },
     {
         id: 3,
         name: "SPF 50 Sunscreen",
         price: "₹399",
-        image: "https://scontent.fpat2-3.fna.fbcdn.net/v/t1.15752-9/531603821_620300137504338_7924061035243799707_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=107&ccb=1-7&_nc_sid=0024fc&_nc_ohc=H13rZ6MYTPQQ7kNvwErjapl&_nc_oc=AdmXOGjQOfYWjdxw4wA9xhwRaT6EgMmRjJzs7simQuD-5idsM_ZOv4Mclp-mv_DATQ4&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-3.fna&oh=03_Q7cD3AG2qDU0EdLsqOYg9Av0ieXgpzvH1O6Yhm_yeaVUDqAQRg&oe=68CB8452",
+        image: "https://scontent.fpat2-1.fna.fbcdn.net/v/t1.15752-9/589652273_1377944220393944_2567216745445574492_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=102&ccb=1-7&_nc_sid=0024fc&_nc_ohc=O3h8Nl-m8v0Q7kNvwEkCNTA&_nc_oc=AdnoFJBBU0a5waHe6IMcFui-rmm9ebFcwYw5C7EyrVxm8d6TePZxmUNJM2QqDDlWxSA&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-1.fna&oh=03_Q7cD4AHLj4HpXqJcu2eFzVfGULIKjkIfWM3vyX72JyVfEAo2-w&oe=6954D34B",
     },
     {
         id: 4,
         name: "Aloe Vera Gel",
         price: "₹199",
-        image: "https://scontent.fpat2-3.fna.fbcdn.net/v/t1.15752-9/530800190_678636025343389_2329368944806633506_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=111&ccb=1-7&_nc_sid=0024fc&_nc_ohc=hAJLAls7utgQ7kNvwG-LDe9&_nc_oc=Adl3tj8arWh3GYblwxWdoHv8b6Gty3aHzdHtcQmG7wazxsxmVeoZnubw-l8-9u-6ztw&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-3.fna&oh=03_Q7cD3AEtL_O3uFU23l0NSdF8rcKc323slTwSfTo_CHBbEUwWYg&oe=68CB894A",
+        image: "https://scontent.fpat2-5.fna.fbcdn.net/v/t1.15752-9/591119338_25162500766741705_2399292631785516151_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=108&ccb=1-7&_nc_sid=0024fc&_nc_ohc=s4mPIG00DT4Q7kNvwEKSgXr&_nc_oc=Adn0F5h0irnM8NwKNMc_9E3WZh5CwP-SbT81Q4VimA1OQScStmUCoFXDo_9xw8fe5hk&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-5.fna&oh=03_Q7cD4AE2ft87QMd474ejcDZ8_PNhCwPGaAgKtlmBdbXXXZroFg&oe=6954CFDA",
     },
     {
         id: 5,
         name: "Night Repair Cream",
         price: "₹699",
-        image: "https://scontent.fpat2-7.fna.fbcdn.net/v/t1.15752-9/535074845_800469805977706_2028757668689890252_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=101&ccb=1-7&_nc_sid=0024fc&_nc_ohc=NviLtkKizD4Q7kNvwF0lc8s&_nc_oc=AdlnR3ajR7h_b5_jRXljbYNHYjfPd_hrkUvJJzEbi_4OvJTOPK5cf46GbPNys5qC0BQ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-7.fna&oh=03_Q7cD3AE25wQCOL-xxo0OqSvsZ0fx93JJngTHpVzRT4rI7rGMmg&oe=68CBACDE",
+        image: "https://scontent.fpat2-1.fna.fbcdn.net/v/t1.15752-9/582058050_794097906996789_5321712931935506947_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=102&ccb=1-7&_nc_sid=0024fc&_nc_ohc=Yri-VvfuEzkQ7kNvwEKPmRN&_nc_oc=AdldQboqgtijZywPNseg70sl1ojuHSVXDS71KNEbNEMr_kCOKwj-nrXgNn6Xe7kjA4M&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-1.fna&oh=03_Q7cD4AGohTQ5n7Jz1BrfBgYKbD1TZci_SdKVbyHxeKvNHMDm_g&oe=6954E7BD",
     },
     {
         id: 6,
         name: "Vitamin C Serum",
         price: "₹899",
-        image: "https://scontent.fpat2-1.fna.fbcdn.net/v/t1.15752-9/532933473_1035624178449502_5758931113824677416_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=103&ccb=1-7&_nc_sid=0024fc&_nc_ohc=ra8sTw2qKpIQ7kNvwH-17Kc&_nc_oc=AdlQyd-RdCCTpNLNDJSrKDtR16zK26doFVizhAaDbVfUwASbJFf_p_ChjMafkCPTQZQ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-1.fna&oh=03_Q7cD3AEOI_1ojA5A6Wauz_80x-nZIuLRFylnUEd7Jn4agKeXXg&oe=68CB9F9A",
+        image: "https://scontent.fpat2-1.fna.fbcdn.net/v/t1.15752-9/567619976_1183165487337275_8250111581632001072_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=102&ccb=1-7&_nc_sid=0024fc&_nc_ohc=7UZng4potqkQ7kNvwFXUncA&_nc_oc=Adk2amb1P8qNOuRm46ztOFxA0U9SFOUm5eLSW-mFXuyqkWhJsl_nKKcuIq5KVj5qc0k&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-1.fna&oh=03_Q7cD4AF6o4wpbzzqEeQcWkN11qn6v542R0wyj5zfTL-ctH1gew&oe=6954E7FF",
     },
 ];
 
