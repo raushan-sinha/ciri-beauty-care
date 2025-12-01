731dd4633eed1aea039ce91013ef08040d3b7b32
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-01 16:55:42 +0530

    Added images links

--- src/data/menProductsData.js
+++ src/data/menProductsData.js
@@ -3,37 +3,37 @@ const menProducts = [
         id: 1,
         name: "Charcoal Face Wash",
         price: "₹349",
-        image: "https://scontent.fpat2-1.fna.fbcdn.net/v/t1.15752-9/536128680_2192542931230342_4403556090168248158_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=102&ccb=1-7&_nc_sid=0024fc&_nc_ohc=HvTRAozu-2oQ7kNvwFr2VOV&_nc_oc=AdmSRrRQahmr7UfznrSv38GFf9wiimIybmLziHtgVjO87LrvmoBLp4hgE6G_DXr36Ak&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-1.fna&oh=03_Q7cD3AGO4gyjNrZPs8zkcPh5ofOzGWRe8tGVS2avRaCk0gGl5Q&oe=68CBCB90",
+        image: "https://scontent.fpat2-3.fna.fbcdn.net/v/t1.15752-9/582204437_1179735220754998_5001485128472778416_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=107&ccb=1-7&_nc_sid=0024fc&_nc_ohc=Rb6wv3i4sqsQ7kNvwHogb4N&_nc_oc=AdkzYwQ1BchwGPE-19X8vpkEqvmNBW2LaF5czOka8QAf7xwr062nC6phKd0sCTBKkic&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-3.fna&oh=03_Q7cD4AHWe2JszuXNly6v6oovuJrSHiVlbrUef_c52xUXw-mdjg&oe=6954E517",
     },
     {
         id: 2,
         name: "Beard Oil",
         price: "₹499",
-        image: "https://scontent.fpat2-1.fna.fbcdn.net/v/t1.15752-9/535543839_2807920636263956_6616213080731611444_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=102&ccb=1-7&_nc_sid=0024fc&_nc_ohc=FJfiqgFp-8wQ7kNvwG9SLyT&_nc_oc=AdnZPKAapYbQ3021njrrvymWGUgHsJc_-aE7cD96WQwsQ3-SmuBBd_SmzLGw5xgTio0&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-1.fna&oh=03_Q7cD3AETWvUADdm5tvGT4wPRNRASzSzEg-0gOn7An7ZF1Wgp3w&oe=68CBA2C4",
+        image: "https://scontent.fpat2-7.fna.fbcdn.net/v/t1.15752-9/575872855_1986721228895246_1874243543908171886_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=101&ccb=1-7&_nc_sid=0024fc&_nc_ohc=dwIY0nICaO0Q7kNvwFInyum&_nc_oc=AdklU7oVzG_-nh_Z-iFSgRpc-s_Kyap4JOAJj8mPGRv22EXoqyv81xZG6LPy8lAs7o4&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-7.fna&oh=03_Q7cD4AFv6-rWrFGbh5WzuY4H59AwckQlCMrjnrHGUgR_m7gyww&oe=6954F352",
     },
     {
         id: 3,
         name: "Shaving Foam",
         price: "₹299",
-        image: "https://scontent.fpat2-7.fna.fbcdn.net/v/t1.15752-9/530689057_1276423177197046_903012005135253052_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=IDeVsD_UsngQ7kNvwEhi0_9&_nc_oc=AdmCInwi4i2PsyW2KAQMG6z2-X5sVxI8b0ApbyXRLiI_t44SYl2RvMkO-JmtDasMUVU&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-7.fna&oh=03_Q7cD3AFgoInbJM1_-eZ-2HKRNLMV3r0unxNsH4GPX8uYgO0exw&oe=68CBBFA5",
+        image: "https://scontent.fpat2-8.fna.fbcdn.net/v/t1.15752-9/590801391_876554224953480_3312374704854596275_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=100&ccb=1-7&_nc_sid=0024fc&_nc_ohc=wphXQTe8W1sQ7kNvwGcEYxu&_nc_oc=Adlv7jnFPgXk-7waL_BoeFOZ8YDmdoLWD1VIsG6-yV4E7OPYrFmR5B4kfd2LtjmET14&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-8.fna&oh=03_Q7cD4AHkiuhDlHKxzVLaomQsvfwJoaDqU3V88tc2hGH3Zw8m1A&oe=6954FD68",
     },
     {
         id: 4,
         name: "After Shave Lotion",
         price: "₹399",
-        image: "https://scontent.fpat2-2.fna.fbcdn.net/v/t1.15752-9/532130568_639382788959894_5014921149946596403_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=110&ccb=1-7&_nc_sid=0024fc&_nc_ohc=DybzHI_yspYQ7kNvwH_XOHl&_nc_oc=AdncGJ5sZUW2YzU1inD4W-0ycSEgSEd_5lOtUydAUedL0PkTabJYNenF2pvZNUXmPH0&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-2.fna&oh=03_Q7cD3AFekNX10kodSg1GQvYd69QjEg-wQdlwgz2kAHXeTDblsw&oe=68CB9FA2",
+        image: "https://scontent.fpat2-8.fna.fbcdn.net/v/t1.15752-9/576288541_1246656690854887_2804389813031899924_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=103&ccb=1-7&_nc_sid=0024fc&_nc_ohc=rrgJz1SvyWIQ7kNvwENhRk-&_nc_oc=AdmR53g54ZzYaI4HkyieL_oUxy-ZnxGaAJPAO2ysBnY6vZDSg34QTz11oG4VXA22zjg&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-8.fna&oh=03_Q7cD4AFBIm4n4Z6v1URDBWsVv1G9HfY-ZV1arhnw3hAWFict-Q&oe=69550111",
     },
     {
         id: 5,
         name: "Hair Styling Gel",
         price: "₹249",
-        image: "https://scontent.fpat2-7.fna.fbcdn.net/v/t1.15752-9/533471577_1815493386516370_6234715012492598897_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=c0WMJckAuAMQ7kNvwELXLZ6&_nc_oc=AdkCObd4W0M6fmU151dmiNP8skQ9bEGoctinxRx0CygjnMA5vLSx5t0zd-8rwcgI5Zw&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-7.fna&oh=03_Q7cD3AFSoy4yXAStMwEG1M1DkZxzYH7M3G1FkQSp_JU-BL0Maw&oe=68CBADA1",
+        image: "https://scontent.fpat2-5.fna.fbcdn.net/v/t1.15752-9/588922519_2581817605551393_5415470710273786329_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=104&ccb=1-7&_nc_sid=0024fc&_nc_ohc=C_Cq7XsP5eQQ7kNvwHl01cM&_nc_oc=Adl-BT5nIUFMgGP5HEHUcU5XbyTHbMx4ga1QONai1kbhaDCZuFVM2HIC2CrnVs6bolw&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-5.fna&oh=03_Q7cD4AHLfpoObvWVh4ACD1ZU7jCEHRVPUTBjMy8HmhVS0MlX-w&oe=6954DC6A",
     },
     {
         id: 6,
         name: "De-Tan Face Pack",
         price: "₹599",
-        image: "https://scontent.fpat2-1.fna.fbcdn.net/v/t1.15752-9/535053348_4320408771511462_3296247245903834719_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=102&ccb=1-7&_nc_sid=0024fc&_nc_ohc=HL9sL-YWTykQ7kNvwHby38j&_nc_oc=AdmbGO8sIjpFs8Dx0tQsRdkpg2suIRNLlZviVuj-WHrxlyuI46hduNoM6eM2Kq4Cf70&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fpat2-1.fna&oh=03_Q7cD3AFToa9csocPq0TyXEVAK7_uFLaxW-dnAwSAeZCIR-FXvQ&oe=68CBAD3C",
+        image: "https://scontent.fpat2-5.fna.fbcdn.net/v/t1.15752-9/582545234_1613992856640025_2306137926225402786_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=104&ccb=1-7&_nc_sid=0024fc&_nc_ohc=y1fqZesHsTUQ7kNvwHjgm6B&_nc_oc=AdlVPtTIIuSEUr5oeTFQuqk6eMPRCYNAgmrHhDQS3jMy9W91KL-bAQMTmry-35mSYik&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=scontent.fpat2-5.fna&oh=03_Q7cD4AEiBVLkfOvvX2eB5XZDHCRHhxPeW24epHoIhp8xbZ7fCA&oe=69550291",
     },
 ];
 
